e956bdc2a8153bfa04e243483af2cf1da964b262
Fabian Mooslechner <fabian.moolsechner@student.tugraz.at>
2025-01-06 20:49:33 +0100

    Make it a link

--- src/comp/svg/IndicatorsOverview.vue
+++ src/comp/svg/IndicatorsOverview.vue
@@ -1,12 +1,12 @@
 <template>
-  <div> 
+  <router-link :to="{name: 'indicator', params: {indicatorId: indicator}}">
     <h3 v-if="indicator" class="indicator-name">{{ $store.getters.getShortname(indicator) }}</h3>
     <p v-else>No indicator selected.</p>
     <p class="description">{{ $store.getters.getLabels(indicator).name_a }}</p>
     <p class="description">{{ $store.getters.value(indicator) }}</p>
     <p>---</p>
-    <!--<bar-indicator :topic="indicator.id" />-->
-  </div>
+    <bar-indicator :topic="indicator.id" />
+  </router-link>
 </template>
 
 <script>

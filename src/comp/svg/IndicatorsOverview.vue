da2a21131d6c33ede8379c105c11ebdd073e08da
Bastian Kandlbauer <b.kandlbauer@student.tugraz.at>
2025-01-06 20:59:46 +0100

    added topic

--- src/comp/svg/IndicatorsOverview.vue
+++ src/comp/svg/IndicatorsOverview.vue
@@ -4,13 +4,16 @@
     <p v-else>No indicator selected.</p>
     <p class="description">{{ $store.getters.getLabels(indicator).name_a }}</p>
     <p class="description">{{ $store.getters.value(indicator) }}</p>
-    <p>---</p>
-    <bar-indicator :topic="indicator.id" />
+    
+    <topic-entry-in-out v-for="([topic, io]) in relevantTopics" :key='topic.id' :topic='topic' :in-out="io"/>
   </router-link>
 </template>
 
 <script>
 import BarIndicator from './BarIndicator.vue';
+import TopicsBlock from '../block/TopicsIndicator.vue'
+import TopicEntryInOut from '../block/shared/TopicEntryInOut.vue';
+import Block from '../block/Base.vue'
 
 export default {
   props: ['indicator'],
@@ -18,9 +21,11 @@ export default {
     width: null,
     testId: 0,
   }),
-  components: {
-    BarIndicator,
+  components: { BarIndicator, TopicsBlock, TopicEntryInOut, Block,
   },
+  computed: {
+      relevantTopics: function () { return this.$store.getters.topicsForIndicator(this.indicator).map(topic => [topic, this.$store.getters.getInOutForTopicIndicator(topic, this.indicator)]) ; },
+    },
   mounted() {
   },
   methods: {
